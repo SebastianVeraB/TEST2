54a77f8dcb9368970d4bb1e84bbac8a72e1ce9a0
Your Name <sebastian.vera@oktana.io>
2020-12-09 17:49:12 -0300

    te

--- buildGithubCheckScript.groovy
+++ buildGithubCheckScript.groovy
@@ -64,12 +64,7 @@ def setCheckName(repository, checkName, status, previousDay, requestMethod, comm
         updateCheckRun = ["name":"${checkName}", 
                           "status": "in_progress", 
                           "conclusion":"${status}", 
-                          "completed_at": "${previousDay}",
-                            "actions": [
-                                        "label": "Fix this",
-                                         "description": "Let us fix that for you",
-                                        "identifier": "fix_errors"
-                                        ]]
+                          "completed_at": "${previousDay}"]
 
 
         def url = "https://api.github.com/repos/${ORGANIZATION_NAME}/${repository}/check-runs"
